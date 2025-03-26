752c4a9cf760716f450e7761c6daa33b54492fb7
JakubBilski1 <jakub.bilski@spoko.pl>
2025-03-26 03:18:46 +0100

    app made pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
+  // add any other Next.js config options here
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  ...nextConfig,
+});
